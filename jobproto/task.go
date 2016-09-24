29c3a318835d3db50726c31a9a4fb561ff3573df
Alex Nichol <unixpickle@gmail.com>
2016-09-24 13:34:15 -0400

    drafted file transfer task

--- jobproto/task.go
+++ jobproto/task.go
@@ -1,5 +1,20 @@
 package jobproto
 
+import (
+	"encoding/gob"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"path/filepath"
+)
+
+const transferBufferSize = 65536
+
+func init() {
+	gob.Register(FileTransfer{})
+}
+
 // A Task implements a unit of work.
 // When a Task is sent from a master to a slave, it will
 // serialized using the gob package.
@@ -27,4 +42,98 @@ type TaskChannel interface {
 	// It blocks until a message is received.
 	// It returns io.EOF if the other side has finished.
 	Receive() (interface{}, error)
+}
+
+// FileTransfer is a Task that implements a file transfer
+// between a master and a slave.
+type FileTransfer struct {
+	// If ToSlave is true, the file is being uploaded to
+	// the slave.
+	ToSlave bool
+
+	MasterPath string
+	SlavePath  string
+}
+
+// RunMaster runs the master's end of the file transfer.
+func (f *FileTransfer) RunMaster(ch TaskChannel) error {
+	if f.ToSlave {
+		return f.runSender(f.MasterPath, ch)
+	} else {
+		return f.runReceiver(f.MasterPath, ch)
+	}
+}
+
+// RunSlave runs the slave's end of the file transfer.
+func (f *FileTransfer) RunSlave(root string, ch TaskChannel) error {
+	path := filepath.Join(root, f.SlavePath)
+	if f.ToSlave {
+		return f.runReceiver(path, ch)
+	} else {
+		return f.runSender(path, ch)
+	}
+}
+
+func (f *FileTransfer) runSender(path string, ch TaskChannel) error {
+	file, err := os.Open(path)
+	if err != nil {
+		ch.Send(err)
+		return err
+	}
+	defer file.Close()
+	buf := make([]byte, transferBufferSize)
+	for {
+		n, err := file.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			ch.Send(err)
+			return err
+		} else if n == 0 {
+			continue
+		}
+		if err := ch.Send(buf[:n]); err != nil {
+			return err
+		}
+	}
+}
+
+func (f *FileTransfer) runReceiver(path string, ch TaskChannel) (resErr error) {
+	tempPath := path + fmt.Sprintf("%v", rand.Int63())
+	outFile, err := os.Create(tempPath)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if resErr != nil {
+			outFile.Close()
+			os.Remove(tempPath)
+		}
+	}()
+
+	for {
+		obj, err := ch.Receive()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		data, ok := obj.([]byte)
+		if !ok {
+			errObj, ok1 := obj.(error)
+			if !ok1 {
+				return fmt.Errorf("receiver got unexpected data type: %T", obj)
+			}
+			return fmt.Errorf("sender error: %s", errObj)
+		}
+		if _, err := outFile.Write(data); err != nil {
+			return err
+		}
+	}
+
+	outFile.Close()
+	return os.Rename(tempPath, path)
 }
